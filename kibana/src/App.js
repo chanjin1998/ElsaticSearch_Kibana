d12cc500a79502db3d3f52dd6f6c244a35984a9c
chanjin1998 <chanjin@kookmin.ac.kr>
2023-03-29 18:14:59 +0900

    feat: Kibana Component

---
+++ kibana/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import KibanaDashboard from './KibanaDashboard';
+function App() {
+  return (
+    <div>
+      <h1>Kibana Dashboard</h1>
+      <KibanaDashboard />
+    </div>
+  );
+    
+}
+
+export default App;
