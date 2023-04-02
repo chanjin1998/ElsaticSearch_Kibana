1dbb5a3ea3f137dccf85ace44ab594b60b2e9062
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-02 18:26:12 +0900

    feat: kibana dashboard

--- kibana/src/KibanaDashboard.js
+++ kibana/src/KibanaDashboard.js
@@ -1,12 +1,18 @@
-import React from 'react'
-function KibanaDashboard() {
+import React from 'react';
+import Iframe from 'react-iframe';
+
+const Dashboard = () => {
   return (
-    <iframe
-      src="http://kibana.example.com:5601/app/kibana#/dashboard/797fcd80-d085-11ed-af68-61d805810905"
+    <Iframe
+      url="http://localhost:5601/app/kibana#/dashboard/797fcd80-d085-11ed-af68-61d805810905"
       width="100%"
-      height="600px"
-      frameBorder="0"
+      height="1000px"
+      id="myId"
+      className="myClassname"
+      display="initial"
+      position="relative"
     />
   );
-}
-export default KibanaDashboard;
+};
+
+export default Dashboard;
