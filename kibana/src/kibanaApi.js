bdb42d202d853e82eb62f3136fe3c936385e4846
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-06 17:02:28 +0900

    feat:Api

---
+++ kibana/src/kibanaApi.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// API 토큰
+const API_KEY = 'NE9wWVVZY0IwVVk3QXQzVTBLY0c6RFlYV0RqdXJUYkdkdnRsclp0UWVCdw==';
+
+// REST API 호출
+export const fetchSavedObjects = () => {
+  return axios.get('http://localhost:5601/api/saved_objects/_find', {
+    headers: {
+      'Authorization': `ApiKey ${API_KEY}`
+    }
+  })
+  .then(response => {
+    // API 호출 결과 처리
+    console.log(response.data);
+  })
+  .catch(error => {
+    // 에러 처리
+    console.error(error);
+  });
+}
+
