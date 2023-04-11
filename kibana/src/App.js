eed5e84964c2dbe2843dd748acbb774dbb023f04
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-11 23:48:25 +0900

    kibana

--- kibana/src/App.js
+++ kibana/src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Dashboard from './KibanaDashboard';
+import "./App.css";
+import Dashboard from "./KibanaDashboard";
+import TeachTable from "./TeachTable"
+import Map from "./Map"
+// import Chart from './Chart'
 function App() {
   return (
     <div>
-      {/* <h1>Kibana Dashboard</h1> */}
-      <Dashboard />
+      <div>
+      <TeachTable/>
+      {/* <Dashboard/> */}
+      </div>
+      <div>
+        <Map/>
+      </div>
     </div>
   );
-    
 }
 
 export default App;
